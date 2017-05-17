cc8ec865e4663b8c6497e69c813c97f08d45f7f6
Chris Smeele <cj.smeele@xs4all.nl>
2017-05-17 11:25:03 +0200

    Fixed cmake module glob and added initial PC template

---
+++ modules/template-pc/src/main.cc
@@ -0,0 +1,13 @@
+/**
+ * \file
+ * \brief
+ * \author    Chris Smeele
+ * \copyright Copyright (c) 2017, Chris Smeele. All rights reserved.
+ * \license   MIT. See LICENSE for the full license text.
+ */
+#include <iostream>
+
+int main(int argc, char **argv) {
+    std::cout << "Hoi wereld!\n";
+    return 0;
+}
