cbaa3994a4838cc6ab23fc834811fbe5b22e2ab4
Robert <robert.bezem@gmail.com>
2017-05-18 18:06:29 +0200

    Created regenerator and creator

--- bmptk++.py
+++ bmptk++.py
@@ -1,67 +1,98 @@
 #!/usr/bin/python3
 
 # Copyright (c) 2017, The R2D2 Team.
+import glob
 
 __author__ = 'Chris Smeele'
 
 import os
-import sys
 import shutil
-import re
 import platform
+import argparse
 
 platform_cmakes = {"pc": "",
                    "pi": "",
                    "arduino": "{0}".format("" if platform.system() is not 'Windows' else "-G\"MSYS Makefiles\"")}
 
-if 'create' not in sys.argv:
-    print('usage: ' + sys.argv[0] + ' create')
-    exit(1)
-
 root_dir = 'modules'
-
+running_dir = os.getcwd()
 if not os.path.isdir(root_dir):
     print('Je zit in de verkeerde dir, ga naar de root van de repo')
     exit(1)
 
-module_name = input('Modulenaam (shortname): ')
-if not len(module_name): exit(0)
 
-module_name = module_name.upper()
+def ask_question(question, validator, error):
+    inp = ""
+    while True:
+        inp = input(question)
+        if validator(inp):
+            break
+        else:
+            print(error)
+    return inp
 
-module_dir = root_dir + '/' + module_name
 
-if os.path.isdir(module_dir):
-    print('Modulenaam is al in gebruik')
-    exit(1)
+def create():
+    module_name = ask_question('Modulenaam (shortname): ',
+                               lambda x: len(x) and not os.path.isdir(root_dir + '/' + x.upper()),
+                               "Something is wrong with this name").upper()
+    module_name = module_name.upper()
+    module_dir = root_dir + '/' + module_name
 
-platform = ''
-platform_question_string = 'Platform: [' + "|".join(platform_cmakes) + "] "
-while platform not in platform_cmakes:
-    platform = input(platform_question_string)
+    platform_question_string = 'Platform: [' + "|".join(platform_cmakes) + "] "
+    build_platform = ask_question(platform_question_string, lambda x: x in platform_cmakes,
+                                  "This is not a correct platform")
 
-if not len(platform): exit(0)
+    template_name = 'template-' + build_platform
 
-template_name = 'template-' + platform
+    shutil.copytree(root_dir + '/' + template_name, module_dir)
 
-shutil.copytree(root_dir + '/' + template_name, module_dir)
+    print('Created module at ' + module_dir)
 
-print('Created module at ' + module_dir)
+    os.chdir(module_dir)
+    os.remove('CMakeLists.txt')
 
-os.chdir(module_dir)
-os.remove('CMakeLists.txt')
+    # If only we had sed on Windows...
 
-# If only we had sed on Windows...
+    with open('../' + template_name + '/CMakeLists.txt', 'r') as src:
+        with open('CMakeLists.txt', 'w') as dest:
+            for line in src:
+                print(line.replace(template_name, module_name.lower()), file=dest, end='')
 
-with open('../' + template_name + '/CMakeLists.txt', 'r') as src:
-    with open('CMakeLists.txt', 'w') as dest:
-        for line in src:
-            print(line.replace(template_name, module_name.lower()), file=dest, end='')
+    os.chdir(running_dir)
+    generate_one(module_dir)
+    print('\nModule ' + module_name + ' is gemaakt in ' + module_dir)
 
-os.mkdir('build')
-os.chdir('build')
 
-# Fixme: Pass toolchain flags for arduino / kvasir-toolchain.
-os.system('cmake .. {0}'.format(platform_cmakes[platform]))
+def generate_one(path):
+    build_platform = ''
+    with open(path + '/.bmptkpp', 'r') as bmptkpp:
+        build_platform = bmptkpp.readline().strip()
+    os.mkdir(path + '/build')
+    os.chdir(path + '/build')
+    os.system('cmake .. {0}'.format(platform_cmakes[build_platform]))
+    os.chdir(running_dir)
 
-print('\nModule ' + module_name + ' is gemaakt in ' + module_dir)
+
+def generate():
+    for filename in glob.iglob('modules/**/.bmptkpp', recursive=True):
+        if 'template' not in filename:
+            generate_one(filename.replace("/.bmptkpp", ""))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    subcom = parser.add_subparsers()
+
+    create_command = subcom.add_parser('create')
+    create_command.set_defaults(func=create)
+
+    generate_command = subcom.add_parser('generate')
+    generate_command.set_defaults(func=generate)
+
+    args = parser.parse_args()
+
+    try:
+        args.func()
+    except AttributeError:
+        parser.print_help()
