66fdb1a8d0b9af7a0df849fb83cd4dfa11fabca1
Jeroen Kok <Jeroen.kok@student.hu.nl>
2017-05-22 22:34:36 +0200

    [GAS-01] Tried to apply the code standard as good as possible.

--- modules/GAS/src/Alarm.cc
+++ modules/GAS/src/Alarm.cc
@@ -1,32 +1,30 @@
+/**
+ * \file
+ * \brief     The definitions of the alarm functionality of GAS-01
+ * \author    Jeroen Kok
+ * \copyright Copyright (c) 2017, The R2D2 Team
+ * \license   See LICENSE
+ */
+
+
 #include Alarm.hh
 
 
 
 
-//PIOB->PIO_OER = 0x01 << 27; turning the pin to an output for the LED. Not sure where it needs to run.
-
-
-
-void check_gas_value( int32_t gas_value ) {
-	if( gas_value > gas_value_threshold ) {
-		trigger_alarm();
-	} else {
-		disable_alarm();
-		is_alarm_enabled = false;
-	}
+void checkGasValue(int32_t gas_value) {
+    if (gas_value > gasValueThreshold) {
+        triggerAlarm();
+    } else {
+        disableAlarm();
+    }
 }
 
-void trigger_alarm() {
-	if ( is_alarm_enabled == false ) {
-//		PIOB->PIO_SODR = 0x01 << 27;
-		is_alarm_enabled = true;
-	}
+void triggerAlarm() {
+//  fuction for turning the right pin on
 }
 
 
-void disable_alarm() {
-	if ( is_alarm_enabled == true ) {
-//		PIOB->PIO_CODR = 0x01 << 27;
-		is_alarm_enabled = false;
-	}
+void disableAlarm() {
+//  fuction for turning the right pin off
 }
--- modules/GAS/src/Alarm.hh
+++ modules/GAS/src/Alarm.hh
@@ -1,24 +1,26 @@
+/**
+ * \file
+ * \brief     The declarations of the alarm functionality of GAS-01
+ * \author    Jeroen Kok
+ * \copyright Copyright (c) 2017, The R2D2 Team
+ * \license   See LICENSE
+ */
+
 #pragma once
 
 //#include hwlib.h
 
-// Code standard not followed because llvm.com couldn't be reached.
+/// just a magic number at the moment
+int32_t gasValueThreshold = 100;
 
-//! a boolean
-bool is_alarm_enabled = false;
+/// /brief Check if the gass value is above the threshold then trigger the alarm
+/// Take the gass value and call trigger_alarm() when it's above the
+/// threshold or disable_alarm() when it's below said threshold.
+/// @param gas_value the gas value
+void checkGasValue(int32_t gasValue);
 
-//! just a magic number at the moment
-int32_t gas_value_threshold = 100;
+/// /brief turn the alarm on
+void triggerAlarm();
 
-//! /brief take the gass value and see if it's above the threshold
-//! Take the gass value and call trigger_alarm() when it's above the threshold or disable_alarm() when it's below said threshold
-//! @param gas_value the gas value
-void check_gas_value( int32_t gas_value );
-
-//! /brief turn the alarm on
-//! Only triggers the alarm once
-void trigger_alarm();
-
-//! turn the alarm off
-//! Only disables the alarm once
-void disable_alarm();
+/// turn the alarm off
+void disableAlarm();
