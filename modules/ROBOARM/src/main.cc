629f87ad877e0e0552c18fd101080f7c3cd52574
ManDeJan <janhlsm@gmail.com>
2017-05-23 12:41:13 +0200

    Test with hwlib-string

--- modules/ROBOARM/src/main.cc
+++ modules/ROBOARM/src/main.cc
@@ -1,6 +1,10 @@
 #include <sam3x8e.h>
+#include "wrap-hwlib.hh"
 
 int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
+
+
+
     return 0;
 }
--- modules/ROBOARM/src/parser.cc
+++ modules/ROBOARM/src/parser.cc
@@ -1,8 +1,9 @@
 #include "parser.hh"
 
 Error parseCommand(hwlib::string<12> command) {
-    if (command == "left 420") {
-        // blaze it
+    if (command == "left 42") {
+        // Answers to life
+        return Error::Syntax; // 42 is never the answer
     }
     return Error::Succesful;
 }
