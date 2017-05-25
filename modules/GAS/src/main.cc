e5e656f84a3d31c77c127e8153a5748aa0e1dad1
Chris Smeele <cj.smeele@xs4all.nl>
2017-05-25 16:25:08 +0200

    [GAS-01] WIP hardware SPI

--- modules/GAS/src/main.cc
+++ modules/GAS/src/main.cc
@@ -8,6 +8,7 @@
 #include <memstore.hh>
 #include "wrap-hwlib.hh"
 
+#include "hwlib-due-spi.hh"
 #include "sd-spi.hh"
 
 int main() {
@@ -21,53 +22,38 @@ int main() {
     hwlib::wait_ms(500);
     a.set(0);
 
-    auto sclk = target::pin_out(target::pins::sck);
-    auto mosi = target::pin_out(target::pins::mosi);
-    auto miso = target::pin_in(target::pins::miso);
-    auto cs = target::pin_out(target::pins::d50);
-    auto spiBus = hwlib::spi_bus_bit_banged_sclk_mosi_miso(sclk, mosi, miso);
-    SdSpi sd(cs, spiBus);
+    // auto sclk = target::pin_out(target::pins::sck);
+    // auto mosi = target::pin_out(target::pins::mosi);
+    // auto miso = target::pin_in(target::pins::miso);
+    // auto cs = target::pin_out(target::pins::d50);
+    // auto spiBus = hwlib::spi_bus_bit_banged_sclk_mosi_miso(sclk, mosi, miso);
+    // SdSpi sd(cs, spiBus);
 
     hwlib::cout << "start\r\n";
 
-    // Test MuStore with memory store backend.
-
-    char mem[1024];
-    MuStore::MemStore store(mem, 1024);
+    auto cs = target::pin_out(target::pins::d5); // XXX deliberately not set to actual CS(d10) for hwspi, temporary.
+    target::spi_bus_due spiBus;
+    SdSpi sd(cs, spiBus);
 
     char buf[512] = {"hoi"};
-    store.write(buf);
 
-    buf[0] = '\0';
-
-    store.rewind();
-    store.read(buf);
-
-    hwlib::cout << "read: <" << buf << ">\r\n";
-
-    buf[0] = 'H';
-    store.rewind();
-    store.write(buf);
-
-    buf[0] = '\0';
-
-    store.rewind();
-    auto x = store.read(buf);
+    auto x = sd.read(buf);
 
     hwlib::cout << "read: <" << buf << "> (ret" << x << ")\r\n";
+    
     hwlib::cout << "done\r\n";
 
-    // Test writing to sd card
+    // // Test writing to sd card
 
-    hwlib::cout << "writing to sd card\r\n";
-    char buffer[512] = {"Test string to write\0"};
-    int ret = sd.write(0, buffer);
-    hwlib::cout << "written with return code: " << ret << "\r\n";
+    // hwlib::cout << "writing to sd card\r\n";
+    // char buffer[512] = {"Test string to write\0"};
+    // int ret = sd.write(0, buffer);
+    // hwlib::cout << "written with return code: " << ret << "\r\n";
 
     while (true) {
-        hwlib::wait_ms(200);
+        hwlib::wait_ms(100);
         a.set(0);
-        hwlib::wait_ms(500);
+        hwlib::wait_ms(100);
         a.set(1);
     }
 
