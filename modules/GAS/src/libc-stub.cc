e04c3209d045f9f8ba469da9f7af829ca1a8966c
Chris Smeele <cj.smeele@xs4all.nl>
2017-05-25 12:51:20 +0200

    [GAS-01] Got store lib working

--- modules/GAS/src/libc-stub.cc
+++ modules/GAS/src/libc-stub.cc
@@ -1,3 +1,39 @@
+/**
+ * \file
+ * \brief     Libc stub code
+ * \author    Chris Smeele
+ * \copyright Copyright (c) 2017, The R2D2 Team
+ * \license   See LICENSE
+ */
 // Pretend that we have a cstdlib to please kvasir-toolchain.
 extern "C" void __libc_init_array();
 extern "C" void __libc_init_array() { }
+
+// Required by MuStore. {{{
+
+// Whoops... we can't get size_t from cstdlib.
+using size_t = unsigned int;
+
+extern "C" void *memset(void *m, int v, size_t size) {
+    char *p = static_cast<char*>(m);
+    for (; size; --size)
+        *(p++) = v;
+    return m;
+}
+
+extern "C" void *memcpy(void *dst, const void *src, size_t size) {
+          char *dstp = static_cast<char*>(dst);
+    const char *srcp = static_cast<const char*>(src);
+
+    for (size_t i = 0; i < size; ++i)
+        dstp[i] = srcp[i];
+
+    return dst;
+}
+
+// }}}
+
+// Needed for virtual destructors.
+// See https://stackoverflow.com/a/31686672/1577347
+void operator delete(void *p, size_t x) throw() { /*while (true);*/ }
+void operator delete(void *p)           throw() { /*while (true);*/ }
--- modules/GAS/src/main.cc
+++ modules/GAS/src/main.cc
@@ -1,6 +1,59 @@
-#include <sam3x8e.h>
+/**
+ * \file
+ * \brief     GAS module main
+ * \author    Chris Smeele
+ * \copyright Copyright (c) 2017, The R2D2 Team
+ * \license   See LICENSE
+ */
+#include <memstore.hh>
+#include "wrap-hwlib.hh"
 
 int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
+
+    namespace target = hwlib::target;
+    auto a = target::pin_out(target::pins::d13);
+    a.set(0);
+    hwlib::wait_ms(500);
+    a.set(1);
+    hwlib::wait_ms(500);
+    a.set(0);
+
+    hwlib::cout << "start\r\n";
+
+    // Test MuStore with memory store backend.
+
+    char mem[1024];
+    MuStore::MemStore store(mem, 1024);
+
+    char buf[512] = {"hoi"};
+    store.write(buf);
+
+    buf[0] = '\0';
+
+    store.rewind();
+    store.read(buf);
+
+    hwlib::cout << "read: <" << buf << ">\r\n";
+
+    buf[0] = 'H';
+    store.rewind();
+    store.write(buf);
+
+    buf[0] = '\0';
+
+    store.rewind();
+    auto x = store.read(buf);
+
+    hwlib::cout << "read: <" << buf << "> (ret" << x << ")\r\n";
+    hwlib::cout << "done\r\n";
+
+    while (true) {
+        hwlib::wait_ms(200);
+        a.set(0);
+        hwlib::wait_ms(500);
+        a.set(1);
+    }
+
     return 0;
 }
