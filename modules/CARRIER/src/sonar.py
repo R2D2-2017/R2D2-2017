f1dcb6d63c70bc8f5acb3ce0d99ff6402425bd29
wartaal55 <wilco@louwerse.com>
2017-05-29 15:51:33 +0200

    Added documentation for sonar.py

--- modules/CARRIER/src/sonar.py
+++ modules/CARRIER/src/sonar.py
@@ -1,18 +1,26 @@
+"""@
+This is a test code for the sonar sensor, using a function (GetSonarDistance) to measure the distance to an object.
+"""
 import RPi.GPIO as GPIO                    #Import GPIO library
 import time                                #Import time library
 GPIO.setmode(GPIO.BCM)                     #Set GPIO pin numbering 
-GPIO.setup(23,GPIO.OUT)                  	#Set pin as GPIO out
-GPIO.setup(24,GPIO.IN)                   	#Set pin as GPIO in
+GPIO.setup(23,GPIO.OUT)                    #Set pin as GPIO out
+GPIO.setup(24,GPIO.IN)                     #Set pin as GPIO in
 
 
-
-def Sonar(trigger, echo):                            	#Associate pin 23 to trigger and 24 to ECHO
-	GPIO.output(trigger, False)                 	#Set TRIG as LOW
-	time.sleep(0.001)                            	#Delay of x seconds
+def GetSonarDistance(trigger, echo):            #Associate pin 23 to trigger and 24 to ECHO
+	"""
+	A function that returns the distance to the nearest object in cm, measured by the sonar sensor.
+	trigger = the trigger pin, the sonar sends a signal through this.
+	echo = the echo pin,  the sonar receives a signal through this.
+	return = the distance in cm or an 404 when no object is detected in range.
+	"""
+	GPIO.output(trigger, False)                 #Set TRIG as LOW
+	time.sleep(0.001)                           #Delay of x seconds
   
-	GPIO.output(trigger, True)                  	#Set TRIG as HIGH
-	time.sleep(0.001)                  	   	#Delay of 0.00001 seconds
-	GPIO.output(trigger, False)                 	#Set TRIG as LOW
+	GPIO.output(trigger, True)                  #Set TRIG as HIGH
+	time.sleep(0.001)                  	   		#Delay of 0.00001 seconds
+	GPIO.output(trigger, False)                 #Set TRIG as LOW
 	
 	
 	while GPIO.input(echo)==0:               	#Check whether the ECHO is LOW
@@ -31,14 +39,18 @@ def Sonar(trigger, echo):                            	#Associate pin 23 to trigg
    		return 404                  		#display out of range
 
 
-
 while True:
-	
-	
-	distance = Sonar(23, 24)
+	"""
+	a simple while loop for testing the GetSonarDistance function.
+	"""
+	distance = GetSonarDistance(23, 24)
 	if(distance<3):
 		print("FORCE STOP", distance)
 	elif(distance<25):
 		print("STOP: ", distance)
 
 	time.sleep(0.01)
+	
+	
+	
+	
