4f59ecc06d02deeeb882b4b2ef6b1e8ec5ef3bac
aglatar <koendg@ziggo.nl>
2017-05-29 21:04:56 +0200

    [RFID 01]renamed file names to fit codeing guidelines

---
+++ modules/RFID/src/led-controller.cc
@@ -0,0 +1,17 @@
+/**
+ * \LedController.cc
+ * \author    Koen de Groot
+ * \copyright Copyright (c) 2017, The R2D2 Team
+ * \license   See LICENSE
+ */
+
+#include "led-controller.hh"
+
+void blinkLed(int pin,int time)
+{
+    pinMode(pin,OUTPUTE);   //set GPIO pin to outpute
+    digitalWrite(pin,1);    //turns led on
+    delay(time);            //delays for time in ms
+    digitalWrite(pin,0);    //turns led off
+}
+ 
