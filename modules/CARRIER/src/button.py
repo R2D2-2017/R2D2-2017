85aff0e7837590ffdcc74e26970e9c92f80ad739
Nerinai <david.dm.dejong@student.hu.nl>
2017-05-29 18:16:16 +0000

    test button

--- modules/CARRIER/src/button.py
+++ modules/CARRIER/src/button.py
@@ -1,41 +1,43 @@
 # External module imports
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 # Button Class
 class Button:
 	# Constructor requires number of pin
 	def __init__(self, pin):
 		self.buttonPin = pin
-		GPIO.setup(self.buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		GPIO.setup(self.buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 	# Check if button pressed
 	def isPressed(self):
 		return GPIO.input(self.buttonPin)
 
 # Buttons
-buttonUp = Button(27)
-buttonDown = Button(16)
-buttonLeft = Button(17)
-buttonRight = Button(18)
+buttonUp = Button(17)
+#buttonDown = Button(16)
+#buttonLeft = Button(17)
+#buttonRight = Button(18)
 lastButton = ""
-
 # Loop for test button
 while True:
 	if buttonUp.isPressed() and lastButton != "UP":
 		print("button up pressed")
-		lastButton = "UP"
-		
-	elif buttonDown.isPressed() and lastButton != "DOWN":
-		print("button down pressed")
-		lastButton = "DOWN"
-		
-	elif buttonRight.isPressed() and lastButton != "RIGHT":
-		print("button right pressed")
-		lastButton = "RIGHT"
-		
-	elif buttonLeft.isPressed() and lastButton != "LEFT":
-		print("button left pressed")
-		lastButton = "LEFT"
+                lastButton = "UP"	
+#	elif buttonDown.isPressed() and lastButton != "DOWN":
+#		print("button down pressed")
+#		lastButton = "DOWN"
+#		
+#	elif buttonRight.isPressed() and lastButton != "RIGHT":
+#		print("button right pressed")
+#		lastButton = "RIGHT"
+#		
+#	elif buttonLeft.isPressed() and lastButton != "LEFT":
+#		print("button left pressed")
+#		lastButton = "LEFT"
 	else:
-		print("nothing is pressed")
+		print("NOTHING PRESSED \n")
+
+
+
+
