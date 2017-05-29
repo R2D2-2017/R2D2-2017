d9e215eea8ea06a6122250e747402cf860edb8b1
woodage <r-valkenburg@hotmail.com>
2017-05-29 22:54:26 +0200

    HallSensor working

--- modules/CARRIER/src/hallsensor.py
+++ modules/CARRIER/src/hallsensor.py
@@ -1,15 +1,18 @@
 # External module imports
 import RPi.GPIO as GPIO
 import time	
-	
+GPIO.setmode(GPIO.BOARD)
+#class HallSensor
 class HallSensor:
-
+	# Constructor for giving input pin
 	def __init__(self, pin):
 		self.hallSensorPin = pin
-		GPIO.setup(hallSensorPin, GPIO.IN)
-		
+		GPIO.setup(self.hallSensorPin, GPIO.IN)
+	# Return status of magnet detection	
 	def isMagnetDetected(self):
-		return return GPIO.input(hallSensorPin)
+		return GPIO.input(self.hallSensorPin)
 		
-hallSensor = HallSensor(15)
-hallSensor.isMagnetDetected()
+# Test code		
+hallSensor = HallSensor(7)
+while True:
+	print(hallSensor.isMagnetDetected())
