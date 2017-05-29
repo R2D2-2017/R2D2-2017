e647ecda990efd0249e0cbbce6102e57fc324f92
Nerinai <david.dm.dejong@student.hu.nl>
2017-05-29 17:09:34 +0000

    [carrier-01]commit because i want to swich branches.

--- modules/CARRIER/src/button.py
+++ modules/CARRIER/src/button.py
@@ -1,19 +1,20 @@
 # External module imports
 import RPi.GPIO as GPIO
 import time
+GPIO.setmode(GPIO.BOARD)
 
 # Button Class
 class Button:
 	# Constructor requires number of pin
 	def __init__(self, pin):
 		self.buttonPin = pin
-		GPIO.setup(buttonUpPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		GPIO.setup(self.buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	# Check if button pressed
 	def isPressed(self):
 		return GPIO.input(self.buttonPin)
 
 # Buttons
-buttonUp = Button(19)
+buttonUp = Button(27)
 buttonDown = Button(16)
 buttonLeft = Button(17)
 buttonRight = Button(18)
@@ -21,19 +22,19 @@ lastButton = ""
 
 # Loop for test button
 while True:
-	if buttonUp.isPressed() AND lastButton != "UP":
+	if buttonUp.isPressed() and lastButton != "UP":
 		print("button up pressed")
 		lastButton = "UP"
 		
-	elif buttonDown.isPressed() AND lastButton != "DOWN":
+	elif buttonDown.isPressed() and lastButton != "DOWN":
 		print("button down pressed")
 		lastButton = "DOWN"
 		
-	elif buttonRight.isPressed() AND lastButton != "RIGHT":
+	elif buttonRight.isPressed() and lastButton != "RIGHT":
 		print("button right pressed")
 		lastButton = "RIGHT"
 		
-	elif buttonLeft.isPressed() AND lastButton != "LEFT":
+	elif buttonLeft.isPressed() and lastButton != "LEFT":
 		print("button left pressed")
 		lastButton = "LEFT"
 	else:
