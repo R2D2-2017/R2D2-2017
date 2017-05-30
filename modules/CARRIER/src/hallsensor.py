485a0b510afceb135d97cde1b66df2b7d87cd994
wartaal55 <wilco@louwerse.com>
2017-05-30 14:47:30 +0200

    Added documentation for hallsensor.py

--- modules/CARRIER/src/hallsensor.py
+++ modules/CARRIER/src/hallsensor.py
@@ -1,18 +1,41 @@
+"""
+\file
+\brief		This is a module for reading the KY-024 Hall Sensor.
+\author		Robbie Valkenburg and Wilco Louwerse.
+\copyright	Copyright (c) 2017, The R2D2 Team.
+\license	See LICENSE.
+\wiki		https://github.com/R2D2-2017/R2D2-2017/wiki/Keyes-KY-024-Hall-Sensor
+\Trello		https://trello.com/c/5KfmCbD0/6-4-install-the-hal-sensors-so-they-can-detect-the-magnets
+"""
+
 # External module imports
 import RPi.GPIO as GPIO
 import time	
 GPIO.setmode(GPIO.BOARD)
-#class HallSensor
+
 class HallSensor:
-	# Constructor for giving input pin
+"""
+\brief A class for reading if the hall sensor detected any form of magnetism.
+"""
 	def __init__(self, pin):
+		""" 
+		\brief Setup HallSensor class with a given pin.
+		
+		\param[in]	pin		the hall sensor pin to be read.
+		"""
 		self.hallSensorPin = pin
 		GPIO.setup(self.hallSensorPin, GPIO.IN)
-	# Return status of magnet detection	
 	def isMagnetDetected(self):
+		""" 
+		\brief This function checks if the hall sensor detected magnetism.
+		
+		\return Returns true if the hall sensor detected magnetism.
+		"""
 		return GPIO.input(self.hallSensorPin)
 		
-# Test code		
+# Create hall sensor class
 hallSensor = HallSensor(7)
+
+# Loop for testing the hall sensor
 while True:
 	print(hallSensor.isMagnetDetected())
