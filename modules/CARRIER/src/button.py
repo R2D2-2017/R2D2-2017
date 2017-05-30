8c58864f724bb74bb2b268515bc6c5e0b0c1b198
wartaal55 <wilco@louwerse.com>
2017-05-30 14:30:01 +0200

    Updated documentation for button.py

--- modules/CARRIER/src/button.py
+++ modules/CARRIER/src/button.py
@@ -1,8 +1,10 @@
-"""@
-Authors: Robbie Valkenburg and Wilco Louwerse
-Wiki: (part of:) https://github.com/R2D2-2017/R2D2-2017/wiki/Carrier-Platform
-Trello: https://trello.com/c/R10Cnnf0/5-4-signals-from-the-buttons-are-now-able-to-control-the-platform
-This is a test code for reading button presses, using a button class that contains a function (isPressed) to check if a button is pressed.
+"""
+\file
+\brief		This is a module for reading button presses.
+\author		Robbie Valkenburg and Wilco Louwerse.
+\copyright	Copyright (c) 2017, The R2D2 Team.
+\license	See LICENSE.
+\Trello		https://trello.com/c/R10Cnnf0/5-4-signals-from-the-buttons-are-now-able-to-control-the-platform
 """
 
 # External module imports
@@ -10,28 +12,34 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 
-# Button Class
 class Button:
 """
-A class for one button with a function (isPressed) to check if a button is pressed
+\brief A class for reading presses of one button.
 """
-	# Constructor requires number of pin
 	def __init__(self, pin):
-		""" Setup button class with a given pin """
+		""" 
+		\brief Setup button class with a given pin.
+		
+		\param[in]	pin		the button pin to be read.
+		"""
 		self.buttonPin = pin
 		GPIO.setup(self.buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-	# Check if button pressed
 	def isPressed(self):
-		""" Returns true if the button is pressed """
+		""" 
+		\brief This function checks if the button is pressed.
+		
+		\return Returns true if the button is pressed.
+		"""
 		return GPIO.input(self.buttonPin)
 
-# Buttons
+# Create button classes
 buttonUp = Button(17)
 buttonDown = Button(16)
 buttonLeft = Button(17)
 buttonRight = Button(18)
 lastButton = ""
-# Loop for test button
+
+# Loop for testing the buttons
 while True:
 	if buttonUp.isPressed() and lastButton != "UP":
 		print("button up pressed")
