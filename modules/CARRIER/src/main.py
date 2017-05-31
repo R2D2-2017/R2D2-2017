994b356d285f083430e4127cb3eae536723af9ca
Nerinai <david.dm.dejong@student.hu.nl>
2017-05-31 16:33:13 +0200

    [carrier-01]: Made python indentations correct

--- modules/CARRIER/src/main.py
+++ modules/CARRIER/src/main.py
@@ -1,9 +1,9 @@
 ##
 #\file
-#\brief		This file cntains the state machien that controlles the Rosbee via the raspberry Pi
-#\author	Mike Hilhorst, David de Jong, Wilco Louwerse, Robbie Valkenburg, Nicky van Steensel van der Aa, Bram van Bergeijk
-#\copyright	Copyright (c) 2017, The R2D2 Team.
-#\license	See LICENSE.
+#\brief     This file cntains the state machien that controlles the Rosbee via the raspberry Pi
+#\author    Mike Hilhorst, David de Jong, Wilco Louwerse, Robbie Valkenburg, Nicky van Steensel van der Aa, Bram van Bergeijk
+#\copyright Copyright (c) 2017, The R2D2 Team.
+#\license   See LICENSE.
 
 from motor import MotorControl                  #Import Motor Controller
 from sonar import Sonar                         #Import Sonar Controller
@@ -18,11 +18,11 @@ GPIO.setmode(GPIO.BCM)                          #Set board in BCM layout
 ##
 #\brief A enum class that contains the various states of the statemachien 
 class States(Enum):                     
-    idle = 1
-    driving = 2
-    turning = 3
-    sensing = 4
-    shutdown = 5
+	idle = 1
+	driving = 2
+	turning = 3
+	sensing = 4
+	shutdown = 5
 
 # Serialport for communication with the rosbee motor controller 
 s = serial.Serial("/dev/ttyAMA0", 38400, timeout=0.5)
@@ -40,73 +40,73 @@ pressed = 0
 ##
 #\brief The state machien
 while True:
-    if state == States.idle:
-        
-        if pressed == 'f':
-            state = States.driving
+	if state == States.idle:
+		
+		if pressed == 'f':
+			state = States.driving
 
-        elif pressed == 'b':
-            state = States.driving
+		elif pressed == 'b':
+			state = States.driving
 
-        elif pressed == 'r':
-            state = States.turning
+		elif pressed == 'r':
+			state = States.turning
 
-        elif pressed == 'l':
-            state = States.turning
+		elif pressed == 'l':
+			state = States.turning
 
-        elif pressed == 'h':
-            state = States.sensing
+		elif pressed == 'h':
+			state = States.sensing
 
-        elif pressed == 's':
-            state = States.shutdown
-            
-        else:
-            print("prease pressu da buttonu")       #Input
-            pressed = input()
+		elif pressed == 's':
+			state = States.shutdown
+			
+		else:
+			print("prease pressu da buttonu")       #Input
+			pressed = input()
 
-    
-    if state == States.driving:                     # Drive state
+	
+	if state == States.driving:                     # Drive state
   
-        if pressed == 'f':                          # Forwards
-            motor.forward(63)                       # Set motor on speed 63
-        elif pressed == 'b':                        # Backwards
-            motor.reverse(63)
-            
-        for i in range(0, 10):
-            time.sleep(0.2)
-            distance = echoLocation.getDistance()   # Get sonar value
-            print(distance)
-            if distance < 40:                       # If the carrie is closer than 40 cm the loop will break
-                break
-                
-        motor.stop()                                # If the loop in finished or broken the motor stops
-        pressed = 0                                 # Back to default values
-        state = States.idle                         # Back to default values
+		if pressed == 'f':                          # Forwards
+			motor.forward(63)                       # Set motor on speed 63
+		elif pressed == 'b':                        # Backwards
+			motor.reverse(63)
+			
+		for i in range(0, 10):
+			time.sleep(0.2)
+			distance = echoLocation.getDistance()   # Get sonar value
+			print(distance)
+			if distance < 40:                       # If the carrie is closer than 40 cm the loop will break
+				break
+				
+		motor.stop()                                # If the loop in finished or broken the motor stops
+		pressed = 0                                 # Back to default values
+		state = States.idle                         # Back to default values
 
-    
-    if state == States.turning:                     # Truning state
+	
+	if state == States.turning:                     # Truning state
 
-        if pressed == 'r':                          # Right
-            motor.right(63)                 
-            motor.stop()                            # Stop
-        elif pressed == 'l':                        # Left
-            motor.left(63)
-            motor.stop()                            # Stop
-            
-        pressed = 0                                 # Back to default values
-        state = States.idle                         # Back to default values
+		if pressed == 'r':                          # Right
+			motor.right(63)                 
+			motor.stop()                            # Stop
+		elif pressed == 'l':                        # Left
+			motor.left(63)
+			motor.stop()                            # Stop
+			
+		pressed = 0                                 # Back to default values
+		state = States.idle                         # Back to default values
 
-    
-    if state == States.sensing:                     #Hallsensor state
-        for i in range(0, 1000):                    
-            hallSensor.isMagnetDetected()           # Make green led turn on when magneticfield is found, default red turned on
-            time.sleep(0.01)
+	
+	if state == States.sensing:                     #Hallsensor state
+		for i in range(0, 1000):                    
+			hallSensor.isMagnetDetected()           # Make green led turn on when magneticfield is found, default red turned on
+			time.sleep(0.01)
 
-        pressed = 0                                 # Back to default values
-        state = States.idle                         # Back to default values
-        
-    
-    if state == States.shutdown:                    # Shutdown protocol               
+		pressed = 0                                 # Back to default values
+		state = States.idle                         # Back to default values
+		
+	
+	if state == States.shutdown:                    # Shutdown protocol               
 
-        s.close()                                   # Closes the serialport
-        exit()                                      # Exit's the command line
+		s.close()                                   # Closes the serialport
+		exit()                                      # Exit's the command line
--- modules/CARRIER/src/motor.py
+++ modules/CARRIER/src/motor.py
@@ -1,11 +1,11 @@
 ##
 #\file
-#\brief		This is a module for controlling the motors.
-#\author	Mike Hilhorst, David de Jong, Wilco Louwerse.
-#\copyright	Copyright (c) 2017, The R2D2 Team.
-#\license	See LICENSE.
-#\wiki		...
-#\Trello(1)	https://trello.com/c/KL8lOZvJ/2-4-allowing-control-of-the-engines-as-to-spin-them-forwards-and-backwards
+#\brief     This is a module for controlling the motors.
+#\author    Mike Hilhorst, David de Jong, Wilco Louwerse.
+#\copyright Copyright (c) 2017, The R2D2 Team.
+#\license   See LICENSE.
+#\wiki      ...
+#\Trello(1) https://trello.com/c/KL8lOZvJ/2-4-allowing-control-of-the-engines-as-to-spin-them-forwards-and-backwards
 #\Trello(2) https://trello.com/c/N8kg309G/3-1-allowing-the-engines-to-be-set-to-spin-in-opposite-directions-so-the-platform-can-turn
 
 import serial
@@ -25,54 +25,54 @@ import time
 #
 # 0xAA = start bit
 # 0x0A = Device ID
-# 0x08 / 0x0C = rotate motor forward 			(motor 0 / motor 1)
+# 0x08 / 0x0C = rotate motor forward            (motor 0 / motor 1)
 # 0x0A / 0x0E = rotate motor backwards/reversed (motor 0 / motor 1)
 class MotorControl:
-	##
-	#\brief Setup MotorControl class with a given serial port.
-	#
-	#\param	serial		The given serial port used for writing assignments to the motors.
+    ##
+    #\brief Setup MotorControl class with a given serial port.
+    #
+    #\param serial      The given serial port used for writing assignments to the motors.
     def __init__(self, serial):
         self.serialport = serial
 
-	##
-	#\brief This function makes both motors rotate forward with a given speed, making the platfom move forward.
-	#
-	#\param	speed		[0-127], The speed at which both the motors will rotate forward.
+    ##
+    #\brief This function makes both motors rotate forward with a given speed, making the platfom move forward.
+    #
+    #\param speed       [0-127], The speed at which both the motors will rotate forward.
     def forward(self, speed):
         self.serialport.write(bytes([0xAA, 0x0A, 0x0C, speed]))
         self.serialport.write(bytes([0xAA, 0x0A, 0x08, speed]))
-  
-	##
-	#\brief This function makes both motors rotate backward with a given speed, making the platfom move backward.
-	#
-	#\param	speed		[0-127], The speed at which both the motors will rotate backward.
+    
+    ##
+    #\brief This function makes both motors rotate backward with a given speed, making the platfom move backward.
+    #
+    #\param speed       [0-127], The speed at which both the motors will rotate backward.
     def reverse(self, speed):
         self.serialport.write(bytes([0xAA, 0x0A, 0x0A, speed]))
         self.serialport.write(bytes([0xAA, 0x0A, 0x0E, speed]))
 
-	##
-	#\brief This function makes the right motor rotate forward and the left motor backward, both with the same given speed. This makes the platfom turn left.
-	#
-	#\param	speed		[0-127], The speed at which both the motors will rotate.
+    ##
+    #\brief This function makes the right motor rotate forward and the left motor backward, both with the same given speed. This makes the platfom turn left.
+    #
+    #\param speed       [0-127], The speed at which both the motors will rotate.
     def left(self, speed):
         self.serialport.write(bytes([0xAA, 0x0A, 0x08, speed]))
         self.serialport.write(bytes([0xAA, 0x0A, 0x0E, speed]))
-		time.sleep(2)
+        time.sleep(2)
 
     ##
-	#\brief This function makes the left motor rotate forward and the right motor backward, both with the same given speed. This makes the platfom turn right.
-	#
-	#\param	speed		[0-127], The speed at which both the motors will rotate.
+    #\brief This function makes the left motor rotate forward and the right motor backward, both with the same given speed. This makes the platfom turn right.
+    #
+    #\param speed       [0-127], The speed at which both the motors will rotate.
     def right(self, speed):
         self.serialport.write(bytes([0xAA, 0x0A, 0x0A, speed]))
         self.serialport.write(bytes([0xAA, 0x0A, 0x0C, speed]))
-		time.sleep(2)
+        time.sleep(2)
 
-	##
-	#\brief This function sets both motors speed to 0, stopping the platform from moving.
-	#
-	# ...info over bits die verzonden worden...
+    ##
+    #\brief This function sets both motors speed to 0, stopping the platform from moving.
+    #
+    # ...info over bits die verzonden worden...
     def stop(self):
         self.serialport.write(bytes([0xAA, 0x0A, 0x08, 0]))
         self.serialport.write(bytes([0xAA, 0x0A, 0x0C, 0]))
