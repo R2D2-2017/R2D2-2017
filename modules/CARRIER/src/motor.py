6caa8bd99fa1441472359e03f4caa1fc21055751
wartaal55 <wilco@louwerse.com>
2017-05-31 15:07:29 +0200

    Added documentation about the bits send to control the motors in motor.py

--- modules/CARRIER/src/motor.py
+++ modules/CARRIER/src/motor.py
@@ -13,6 +13,20 @@ import time
 
 ##
 #\brief A class for controlling the motors to be able to make the platfom move forward, backward, stop it and make it turn.
+#
+# Further explanation about what is written to the serial port in all the functions this class contains;
+# The first write to the serial port affects the right motor and the second affects the left motor.
+# The first bit send every write is always a start bit (0xAA), followed by a bit that contains the Device ID (0x0A).
+# The third bit after that is the command bit, in this case it makes the motor rotate forward or backward.
+# More detailed information about this command bit can be found here: https://www.pololu.com/docs/0J29/5
+# More detailed information about making the motor rotate forward or backward with this command bit can be found here:
+# https://www.pololu.com/docs/0J29/5.f
+# The fourth and last bit is the bit used to set the motor speed, this can be in a range from 0 to 127.
+#
+# 0xAA = start bit
+# 0x0A = Device ID
+# 0x08 / 0x0C = rotate motor forward 			(motor 0 / motor 1)
+# 0x0A / 0x0E = rotate motor backwards/reversed (motor 0 / motor 1)
 class MotorControl:
 	##
 	#\brief Setup MotorControl class with a given serial port.
@@ -24,7 +38,6 @@ class MotorControl:
 	##
 	#\brief This function makes both motors rotate forward with a given speed, making the platfom move forward.
 	#
-	# ...info over bits die verzonden worden...
 	#\param	speed		[0-127], The speed at which both the motors will rotate forward.
     def forward(self, speed):
         self.serialport.write(bytes([0xAA, 0x0A, 0x08, speed]))
@@ -33,7 +46,6 @@ class MotorControl:
 	##
 	#\brief This function makes both motors rotate backward with a given speed, making the platfom move backward.
 	#
-	# ...info over bits die verzonden worden...
 	#\param	speed		[0-127], The speed at which both the motors will rotate backward.
     def reverse(self, speed):
         self.serialport.write(bytes([0xAA, 0x0A, 0x0A, speed]))
@@ -42,7 +54,6 @@ class MotorControl:
 	##
 	#\brief This function makes the right motor rotate forward and the left motor backward, both with the same given speed. This makes the platfom turn left.
 	#
-	# ...info over bits die verzonden worden...
 	#\param	speed		[0-127], The speed at which both the motors will rotate.
     def left(self, speed):
         self.serialport.write(bytes([0xAA, 0x0A, 0x08, speed]))
@@ -52,7 +63,6 @@ class MotorControl:
     ##
 	#\brief This function makes the left motor rotate forward and the right motor backward, both with the same given speed. This makes the platfom turn right.
 	#
-	# ...info over bits die verzonden worden...
 	#\param	speed		[0-127], The speed at which both the motors will rotate.
     def right(self, speed):
         self.serialport.write(bytes([0xAA, 0x0A, 0x0A, speed]))
@@ -65,4 +75,4 @@ class MotorControl:
 	# ...info over bits die verzonden worden...
     def stop(self):
         self.serialport.write(bytes([0xAA, 0x0A, 0x08, 0]))
-        self.serialport.write(bytes([0xAA, 0x0A, 0x0D, 0]))
+        self.serialport.write(bytes([0xAA, 0x0A, 0x0C, 0]))
