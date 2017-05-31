d6f9fdd974e55bdc348859dbf52c8a07f0a6dca0
Nerinai <david.dm.dejong@student.hu.nl>
2017-05-31 15:20:15 +0200

    [carrier]: Added documentation to main.py

--- modules/CARRIER/src/main.py
+++ modules/CARRIER/src/main.py
@@ -1,33 +1,46 @@
-from motor import MotorControl
-from sonar import Sonar
-from hallsensor import HallSensor
-from enum import Enum
-import RPi.GPIO as GPIO
-import serial
-import time
-GPIO.setmode(GPIO.BCM)
+##
+#\file
+#\brief		This file cntains the state machien that controlles the Rosbee via the raspberry Pi
+#\author	Mike Hilhorst, David de Jong, Wilco Louwerse, Robbie Valkenburg, Nicky van Steensel van der Aa, Bram van Bergeijk
+#\copyright	Copyright (c) 2017, The R2D2 Team.
+#\license	See LICENSE.
 
+from motor import MotorControl                  #Import Motor Controller
+from sonar import Sonar                         #Import Sonar Controller
+from hallsensor import HallSensor               #Import HallSenor Controller
+from enum import Enum                           #Import Enum
+import RPi.GPIO as GPIO                         #Import GPIO lib
+import serial                                   #Import serial lib
+import time                                     #Import time lib
 
-class States(Enum):
+GPIO.setmode(GPIO.BCM)                          #Set board in BCM layout
+
+##
+#\brief A enum class that contains the various states of the statemachien 
+class States(Enum):                     
     idle = 1
     driving = 2
     turning = 3
     sensing = 4
     shutdown = 5
 
+# Serialport for communication with the rosbee motor controller 
 s = serial.Serial("/dev/ttyAMA0", 38400, timeout=0.5)
-motor = MotorControl(s)
 
-echoLocation = Sonar(23, 24) # pin 23 as triggerpin 24 as echopin
-hallSensor = HallSensor(5, 27, 22) # pin 27 as red led and 22 as green led
+# Making object with the correct pins
+motor = MotorControl(s)                         # Serialport
+echoLocation = Sonar(23, 24)                    # Pin 23 as triggerpin 24 as echopin
+hallSensor = HallSensor(5, 27, 22)              # Pin 27 as red led and 22 as green led
 
+# Statemachien default
 state = States.idle
 pressed = 0
 
 
+##
+#\brief The state machien
 while True:
     if state == States.idle:
-        #mockup for button input
         
         if pressed == 'f':
             state = States.driving
@@ -48,48 +61,52 @@ while True:
             state = States.shutdown
             
         else:
-            print("prease pressu da buttonu")
+            print("prease pressu da buttonu")       #Input
             pressed = input()
 
-    if state == States.driving:
+    
+    if state == States.driving:                     # Drive state
   
-        if pressed == 'f':
-            motor.forward(63)
-        elif pressed == 'b':
+        if pressed == 'f':                          # Forwards
+            motor.forward(63)                       # Set motor on speed 63
+        elif pressed == 'b':                        # Backwards
             motor.reverse(63)
             
         for i in range(0, 10):
             time.sleep(0.2)
-            distance = echoLocation.getDistance()
+            distance = echoLocation.getDistance()   # Get sonar value
             print(distance)
-            if distance < 40:
+            if distance < 40:                       # If the carrie is closer than 40 cm the loop will break
                 break
                 
-        motor.stop()
-        pressed = 0
-        state = States.idle
+        motor.stop()                                # If the loop in finished or broken the motor stops
+        pressed = 0                                 # Back to default values
+        state = States.idle                         # Back to default values
 
-    if state == States.turning:
+    
+    if state == States.turning:                     # Truning state
 
-        if pressed == 'r':
-            motor.right(63)
-            motor.stop()
-        elif pressed == 'l':
+        if pressed == 'r':                          # Right
+            motor.right(63)                 
+            motor.stop()                            # Stop
+        elif pressed == 'l':                        # Left
             motor.left(63)
-            motor.stop()
+            motor.stop()                            # Stop
             
-        pressed = 0
-        state = States.idle
+        pressed = 0                                 # Back to default values
+        state = States.idle                         # Back to default values
 
-    if state == States.sensing:
-        for i in range(0, 1000):
-            hallSensor.isMagnetDetected()
+    
+    if state == States.sensing:                     #Hallsensor state
+        for i in range(0, 1000):                    
+            hallSensor.isMagnetDetected()           # Make green led turn on when magneticfield is found, default red turned on
             time.sleep(0.01)
 
-        pressed = 0
-        state = States.idle
+        pressed = 0                                 # Back to default values
+        state = States.idle                         # Back to default values
+        
+    
+    if state == States.shutdown:                    # Shutdown protocol               
 
-    if state == States.shutdown:
-
-        s.close()
-        exit()
+        s.close()                                   # Closes the serialport
+        exit()                                      # Exit's the command line
