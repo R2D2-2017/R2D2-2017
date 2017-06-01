3c6688e9b7e600a4b2b36507b7f3e42711731650
aglatar <koendg@ziggo.nl>
2017-06-01 10:53:15 +0200

    deleted old files

--- modules/RFID/src/main.cc
+++ modules/RFID/src/main.cc
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <wiringPi.h>
-#inlcude <LedController.hh>
+#inlcude <led-controller.hh>
 
 int main (void)
 {
     wiringPiSetup ();
+    LedControler();
     blinkLed(0,1000); // wiringPi pin 0 is BCM_GPIO 17. 1000 ms = 1 sec
 }
