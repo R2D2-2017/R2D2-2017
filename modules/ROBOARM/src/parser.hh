7405220229ff7603028b95e1389a067a29c25afe
ManDeJan <janhlsm@gmail.com>
2017-06-01 10:34:02 +0200

    [ROBOARM] Updated doxygen

--- modules/ROBOARM/src/parser.hh
+++ modules/ROBOARM/src/parser.hh
@@ -9,7 +9,7 @@ enum class Error : uint8_t {
 /**
  * \brief Parses a string and drives the servo motor
  *
- * @param  command    how the servo should be moving as a string eg: "x 42"
- * @return            returns an error code so you can check if the execution was done succefully
+ * @param[in]  command  how the servo should be moving as a string eg: "x 42"
+ * @return              returns an error code so you can check if the execution was done succefully
  */
 Error parseCommand(hwlib::string<0> command);
