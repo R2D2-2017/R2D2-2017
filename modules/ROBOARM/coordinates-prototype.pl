78ad95fbc3a201b013784cbf37a160e732fba963
Chris Smeele <cj.smeele@xs4all.nl>
2017-06-12 23:04:41 +0200

    [ROBOARM-02] Prototype: Naive bounds checking, read coords from cmdline

--- modules/ROBOARM/coordinates-prototype.pl
+++ modules/ROBOARM/coordinates-prototype.pl
@@ -50,6 +50,9 @@ sub arm_move {
     my $arm = shift;
     my @pos = @_;
 
+    # We can't handle exact zero values very well...
+    $_ ||= 0.001 for @pos;
+
     # Quality sketches!
     #
     #       o------<
@@ -60,9 +63,19 @@ sub arm_move {
     #
     #   o-------o-------<
     #
-    
+
     my $distance = sqrt($pos[0]**2 + $pos[1]**2);
     my $max_distance = $arm->{len1} + $arm->{len2};
+    my $min_distance = abs $arm->{len2} - $arm->{len1};
+
+    # Sanity checks.
+
+    die "Provided point " . fmtpos(@pos) . " is out of reach "
+        . sprintf("(distance assertion %.1f <= %.1f <= %.1f)\n", $min_distance, $distance, $max_distance)
+          if $distance > $max_distance or $distance < $min_distance;
+
+    # TODO: Add rotation limits.
+
 
     # Angle between position and ground.
     my $off_rot = atan($pos[1] / $pos[0]);
@@ -81,30 +94,22 @@ sub arm_move {
     say "move to -> " . fmtpos(@pos)
         . " off(" . sprintf("%3.1f", rad2deg $off_rot)
         . ") rot: " . fmtpos(rad2deg($rot1), rad2deg($rot2))
-        . "   dist<$distance>";
+        . sprintf(" dist<%.1f>", $distance);
 
     $arm->{rot1} = $rot1;
     $arm->{rot2} = $rot2;
 }
 
-my %arm = (
-    base => [0, 0], # Not used.
-    len1 => 18.5,   # Length of first arm part.
-    len2 => 20,     # Length of second arm part.
-    rot1 =>  0,     # Rotation of first joint. (from ground _ )
-    rot2 =>  0,     # Rotation of second joint. (from ground _ ) -> Independent from rot1.
-);
-
 sub dump_arm {
     my %arm = @_;
-    say "points at " . fmtpos(@{{ arm_get_positions %arm }->{point1}}) . ", ",
-                       fmtpos(@{{ arm_get_positions %arm }->{point2}});
+    say "joint2 @ " . fmtpos(@{{ arm_get_positions %arm }->{point1}})
+      . ", hand @ " . fmtpos(@{{ arm_get_positions %arm }->{point2}});
 }
 
 sub show_arm {
     my %arm = @_;
 
-    my $scale = 4; # Must be at least 2 to accommodate the font.
+    my $scale = 4; # Must be at least 3 to accommodate the font.
     my $im = new GD::Image($scale*100, $scale*100);
 
     # What are colors /really/ though.
@@ -120,10 +125,12 @@ sub show_arm {
 
     # Draw grid.
     for my $r (0..10) {
+        $im->line(0, $r*10*$scale, 10*10*$scale, $r*10*$scale, $grey);
+        $im->line($r*10*$scale, 0, $r*10*$scale, 10*10*$scale, $grey);
+
         for my $c (0..10) {
-            #$im->setPixel($r*10*$scale, $c*10*$scale, $grey);
-            $im->filledRectangle($r*10*$scale,        $c*10*$scale,
-                                 $r*10*$scale+$scale, $c*10*$scale + $scale,
+            $im->filledRectangle($r*10*$scale-$scale/2,        $c*10*$scale - $scale/2,
+                                 $r*10*$scale+$scale/2, $c*10*$scale + $scale/2,
                                  $grey);
         }
     }
@@ -144,9 +151,10 @@ sub show_arm {
     # Y coordinate 0 is at the top of the canvas. This is annoying.
     $im->flipVertical();
 
-    $im->string(gdGiantFont, 2, 0, "ends at", $black);
-    $im->string(gdGiantFont, 2, 14, fmtpos(@{$pos{point1}}), $red);
-    $im->string(gdGiantFont, 2, 28, fmtpos(@{$pos{point2}}), $green);
+    $im->string(gdGiantFont, 2,  0, fmtpos(@{$pos{point1}}) . " - joint2", $red);
+    $im->string(gdGiantFont, 2, 14, fmtpos(@{$pos{point2}}) . " - hand",   $green);
+    $im->string(gdGiantFont, 2, 28, fmtpos(rad2deg($arm{rot1}), rad2deg($arm{rot2})) . " - rot (j1,j2)", $blue);
+    $im->string(gdGiantFont, 2, 42, "grid cell = 10x10cm", $black);
 
     # Write directly to an image viewer.
     open my $feh, '|-', 'feh -' or die "$!";
@@ -155,14 +163,33 @@ sub show_arm {
     wait; # Wait for feh to die a slow and painful death.
 }
 
-dump_arm %arm; show_arm %arm;
+my %arm = (
+    base => [0, 0], # Not used.
+    len1 => 18.5,   # Length of first arm part.
+    len2 => 20,     # Length of second arm part.
+    rot1 =>  0,     # Rotation of first joint. (from ground _ )
+    rot2 =>  0,     # Rotation of second joint. (from ground _ ) -> Independent from rot1.
+);
 
-# Some tests.
-arm_move(\%arm, 10, 20); dump_arm %arm; show_arm %arm;
-arm_move(\%arm, 20, 10); dump_arm %arm; show_arm %arm;
-arm_move(\%arm, 28, 12); dump_arm %arm; show_arm %arm;
-arm_move(\%arm, 10,  5); dump_arm %arm; show_arm %arm;
-arm_move(\%arm, 15, 10); dump_arm %arm; show_arm %arm;
-arm_move(\%arm, 20,  0); dump_arm %arm; show_arm %arm;
-arm_move(\%arm,  9,  0); dump_arm %arm; show_arm %arm;
-arm_move(\%arm,  4,  0); dump_arm %arm; show_arm %arm;
+if (@ARGV == 2) {
+
+    # Show arm position/rotation for the X and Y provided on the command line.
+
+    arm_move(\%arm, $ARGV[0], $ARGV[1]); dump_arm %arm; show_arm %arm;
+
+} elsif (@ARGV) {
+    die "usage: $0 [x y]\n";
+
+} else {
+    dump_arm %arm; show_arm %arm;
+
+    # Some tests.
+    arm_move(\%arm, 10, 20); dump_arm %arm; show_arm %arm;
+    arm_move(\%arm, 20, 10); dump_arm %arm; show_arm %arm;
+    arm_move(\%arm, 28, 12); dump_arm %arm; show_arm %arm;
+    arm_move(\%arm, 10,  5); dump_arm %arm; show_arm %arm;
+    arm_move(\%arm, 15, 10); dump_arm %arm; show_arm %arm;
+    arm_move(\%arm, 20,  0); dump_arm %arm; show_arm %arm;
+    arm_move(\%arm,  9,  0); dump_arm %arm; show_arm %arm;
+    arm_move(\%arm,  4,  0); dump_arm %arm; show_arm %arm;
+}
