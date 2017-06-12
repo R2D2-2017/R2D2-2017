83cf17b7946a353356fc63bbd7fc0f9d5f5921b1
Arco Gelderblom <arco.gelderblom@ziggo.nl>
2017-06-12 14:20:53 +0200

    Working on a solution for not properly reading the files and extracting nodes/vertices, added file is open check

--- modules/NAVSYS/common/graph-factory.cc
+++ modules/NAVSYS/common/graph-factory.cc
@@ -1,7 +1,7 @@
 /**
  * \file
  * \brief     This file includes contains the implementations for the graphfactory class
- * \author    Jeremy
+ * \author    Jeremy, Arco Gelderblom
  * \copyright Copyright (c) 2017, The R2D2 Team
  * \license   See LICENSE
  */
@@ -35,64 +35,66 @@ std::vector<Node>  GraphFactory::RunNodeFactory(std::string nodeFilePath) {
     std::ifstream nodeFileStreamIn;
     nodeFileStreamIn.open(nodeFilePath,std::ios_base::app);
 
+    if (nodeFileStreamIn.is_open()) {
+        std::string nodeEntry = "";
+        //get line out of file
+        while (getline(nodeFileStreamIn,nodeEntry)) {
+            std::string nodeName = "";
+            std::string nodePosX = "";
+            std::string nodePosY = "";
 
-    std::string nodeEntry = "";
-    //get line out of file
-    while (getline (nodeFileStreamIn,nodeEntry)) {
-        std::string nodeName = "";
-        std::string nodePosX = "";
-        std::string nodePosY = "";
+            // flags to to deside based on specific chars, which data element is being read.
+            bool nameFlag = 0;
+            bool coordinateFlagX = 0;
+            bool coordinateFlagY = 0;
 
-        // flags to to deside based on specific chars, which data element is being read.
-        bool nameFlag = 0;
-        bool coordinateFlagX = 0;
-        bool coordinateFlagY = 0;
+            // for each char in line
+            unsigned int i = 0;
+            while (i < nodeEntry.length()) {
 
-        // for each char in line
-        unsigned int i = 0;
-        while (i < nodeEntry.length()) {
-
-            char c = nodeEntry.at(i);
-            if (c == '(') {
-                nameFlag = 1;
-            }
-            else if (c == ')') {
-                nameFlag = 0;
-            }
-            else if (c == '[') {
-                coordinateFlagX = 1;
-            }
-            else if (c == ']') {
-                coordinateFlagY = 0;
-            }
-            else if (c == ',') {
-                coordinateFlagX = 0;
-                coordinateFlagY = 1;
-            }
-            else {
-                if (nameFlag) {
-                    nodeName += c;
+                char c = nodeEntry.at(i);
+                if (c == '(') {
+                    nameFlag = 1;
                 }
-                if (coordinateFlagX) {
-                    nodePosX += c;
+                else if (c == ')') {
+                    nameFlag = 0;
                 }
-                if (coordinateFlagY) {
-                    nodePosY += c;
+                else if (c == '[') {
+                    coordinateFlagX = 1;
                 }
-
+                else if (c == ']') {
+                    coordinateFlagY = 0;
+                }
+                else if (c == ',') {
+                    coordinateFlagX = 0;
+                    coordinateFlagY = 1;
+                }
+                else {
+                    if (nameFlag) {
+                        nodeName += c;
+                    }
+                    if (coordinateFlagX) {
+                        nodePosX += c;
+                    }
+                    if (coordinateFlagY) {
+                        nodePosY += c;
+                    }
+                }
+                i++;
             }
-            i++;
+
+            float tmpPosX = std::stof(nodePosX);
+            float tmpPosY = std::stof(nodePosY);
+            // add created node to vector
+            Node newNode = Node(tmpPosX,tmpPosY,  nodeName);
+            nodes.push_back(newNode);
+
         }
-
-        float tmpPosX = std::stof(nodePosX);
-        float tmpPosY = std::stof(nodePosY);
-        // add created node to vector
-        Node newNode = Node(tmpPosX,tmpPosY,  nodeName);
-        nodes.push_back(newNode);
-
+        std::cout<<"in RunNodeFactory: " <<nodes.size() <<"\n";
+        nodeFileStreamIn.close();
+    } else {
+        std::cerr<<"Couldn't open the file";
     }
-
-    nodeFileStreamIn.close();
     return nodes;
 }
 
@@ -105,71 +107,76 @@ std::vector<Vertice> GraphFactory::RunVerticeFactory(std::string verticeFilePath
     std::ifstream verticeFileStreamIn;
     verticeFileStreamIn.open(verticeFilePath,std::ios_base::app);
 
-    std::string verticeEntry = "";
-    //get line out of file
-    while (getline (verticeFileStreamIn,verticeEntry)) {
+    if (verticeFileStreamIn.is_open()) {
+        std::string verticeEntry = "";
+        //get line out of file
+        while (getline (verticeFileStreamIn,verticeEntry)) {
 
-        // strings to store chars read
-        std::string nodeA = "";
-        std::string nodeB = "";
-        std::string weight = "";
+            // strings to store chars read
+            std::string nodeA = "";
+            std::string nodeB = "";
+            std::string weight = "";
 
 
-        // flags to to deside based on specific chars, which data element is being read.
-        bool nodeFlagA = 0;
-        bool nodeFlagB = 0;
-        bool weightFlag = 0;
+            // flags to to deside based on specific chars, which data element is being read.
+            bool nodeFlagA = 0;
+            bool nodeFlagB = 0;
+            bool weightFlag = 0;
 
-        // for each char in line
-        unsigned int i = 0;
-        while (i < verticeEntry.length()) {
-            char c = verticeEntry.at(i);
+            // for each char in line
+            unsigned int i = 0;
+            while (i < verticeEntry.length()) {
+                char c = verticeEntry.at(i);
 
-            if (c == '(' && !nodeFlagB) {
-                nodeFlagA = 1;
-            }
-            else if (c == '(' && nodeFlagB) {
-                //nothing
-            }
-            else if (c == ')') {
-                nodeFlagA = 0;
-                nodeFlagB = 0;
-
-            }
-            else if (c == '[') {
-                weightFlag = 1;
-            }
-            else if (c == ']') {
-                weightFlag = 0;
-            }
-            else if (c == '-'){
-                nodeFlagB =1;
-            }
-            else {
-                if (nodeFlagA) {
-                    nodeA += c;
+                if (c == '(' && !nodeFlagB) {
+                    nodeFlagA = 1;
                 }
-                if (nodeFlagB) {
-                    nodeB += c;
+                else if (c == '(' && nodeFlagB) {
+                    //nothing
                 }
-                if (weightFlag) {
-                    weight += c;
+                else if (c == ')') {
+                    nodeFlagA = 0;
+                    nodeFlagB = 0;
+
                 }
+                else if (c == '[') {
+                    weightFlag = 1;
+                }
+                else if (c == ']') {
+                    weightFlag = 0;
+                }
+                else if (c == '-'){
+                    nodeFlagB =1;
+                }
+                else {
+                    if (nodeFlagA) {
+                        nodeA += c;
+                    }
+                    if (nodeFlagB) {
+                        nodeB += c;
+                    }
+                    if (weightFlag) {
+                        weight += c;
+                    }
+                }
+
+                i++;
             }
 
-            i++;
+            int tmpWeight =  atoi(weight.c_str());
+            // add created vertice to vector
+            vertices.push_back(Vertice( Node( getNodeByName(nodeA,nodes).getCoordinate().x, getNodeByName(nodeA,nodes).getCoordinate().y,
+                                      getNodeByName(nodeA,nodes).getName()),
+                                Node( getNodeByName(nodeB,nodes).getCoordinate().x, getNodeByName(nodeB,nodes).getCoordinate().y,
+                                      getNodeByName(nodeB,nodes).getName()),
+                                tmpWeight ));
+
         }
-
-        int tmpWeight =  atoi(weight.c_str());
-        // add created vertice to vector
-        vertices.push_back(Vertice( Node( getNodeByName(nodeA,nodes).getCoordinate().x, getNodeByName(nodeA,nodes).getCoordinate().y,
-                                  getNodeByName(nodeA,nodes).getName()),
-                            Node( getNodeByName(nodeB,nodes).getCoordinate().x, getNodeByName(nodeB,nodes).getCoordinate().y,
-                                  getNodeByName(nodeB,nodes).getName()),
-                            tmpWeight ));
-
+        verticeFileStreamIn.close();
+        std::cout<<"in RunVerticeFactory: " <<vertices.size() <<"\n";
+    } else {
+        std::cerr<<"Couldn't open the file";
     }
-    verticeFileStreamIn.close();
     return vertices;
 }
 
