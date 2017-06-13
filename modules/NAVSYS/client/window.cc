431637378a86417aed4ccc609365545a431787c1
stefan-1212 <stefan.debeer@student.hu.nl>
2017-06-13 14:50:53 +0000

    [NAVSYS-02] Added doxygen documentation to window class and removed unused variable

--- modules/NAVSYS/client/window.cc
+++ modules/NAVSYS/client/window.cc
@@ -1,19 +1,16 @@
 #include "window.hh"
 
 Window::Window(sf::VideoMode mode, const sf::String title, sf::Uint32 style):
-    sf::RenderWindow(mode, title),
-    windowSize(mode.width, mode.height)
+    sf::RenderWindow(mode, title)
 {}
 
 void Window::setViewPort(const sf::Vector2f size, const sf::Vector2f center){
     view.setSize(size.x, size.y);
     view.setCenter(center);
-    setView(view);
 }
 
 void Window::setViewPos(const sf::Vector2f pos){
     view.setCenter(sf::Vector2f((int)pos.x, (int)pos.y));
-    setView(view);
 }
 
 void Window::moveViewPort(const sf::Vector2f distance){
