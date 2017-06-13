05fae6783b0876d4e1c4c2e7ac7faf977d8f3817
ADvanbergeijk <advanbergeijk@gmaill.com>
2017-06-13 16:38:25 +0200

    [GAS-02] Added function to convert the integer value of the sensors to a character array.

--- modules/GAS/src/main.cc
+++ modules/GAS/src/main.cc
@@ -4,7 +4,7 @@
  * \author    Chris Smeele
  * \author    David Driessen
  * \author    Paul Ettema
- * \author	  Bram van bergeijk
+ * \author	  Bram van Btrgeijk
  * \author	  Wilco Louwerse
  * \copyright Copyright (c) 2017, The R2D2 Team
  * \license   See LICENSE
@@ -16,6 +16,20 @@
 #include "data-logger.hh"
 #include "alarm.hh"
 #include "mq5.hh"
+
+/**
+ * \brief Casts int value of maximum 3 numbers to characters,
+ *		  The characters are stored in the second parameter
+ *		  mq5Char.
+ * \returns Void
+ */
+void convertToChar(int mq5Value, char mq5Char[3]);
+
+void convertToChar(int mq5Value, char mq5Char[3]){
+    mq5Char[0] = ((char)(mq5Value / 100 % 10) + '0');
+    mq5Char[1] = ((char)(mq5Value % 100 / 10) + '0');
+    mq5Char[2] = ((char)(mq5Value % 10 ) + '0');
+}
 
 int main(){
     namespace target = hwlib::target;
@@ -32,10 +46,10 @@ int main(){
     // Initialize classes
     SdSpi sd(cs, spiBus);
     auto logger = DataLogger(sd);
-
     Alarm alarm = Alarm(2.7f, alarmled);
     Mq5 mq5 = Mq5(sensor);
     int mq5Value = 0;
+    char charValue[3];
     // Startup blink
     a.set(0);
     hwlib::wait_ms(200);
@@ -48,7 +62,7 @@ int main(){
         uint64_t time = hwlib::now_us();
         // For debugging print a . for each measurement
         mq5Value = mq5.getSensorPercentage();
-        cout << mq5Value << "%" << "\t";
+        convertToChar(mq5Value, charValue);
         logger.writeValue(mq5Value);
         alarm.checkGasValue(mq5Value);
 
