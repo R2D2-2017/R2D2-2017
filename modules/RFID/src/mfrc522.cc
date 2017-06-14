a010ecd1409b2aa0db0e7dec1b8f7c514a55cb70
philippezwietering <philippe.zwietering@gmail.com>
2017-06-14 13:07:56 +0200

    [RFID-02] Forgot namespace

--- modules/RFID/src/mfrc522.cc
+++ modules/RFID/src/mfrc522.cc
@@ -138,7 +138,7 @@ bool Mfrc522::isTagPresent(){
     return status == statusCodes::statusOk;
 }
 
-bool receiveTagId(uint8_t * inputForId){
+bool Mfrc522::receiveTagId(uint8_t * inputForId){
     writeRegister(mfrc522Registers::bitFraming, 0x07);
 
     uint8_t data = static_cast<uint8_t>(mifareCommands::antiColl);
