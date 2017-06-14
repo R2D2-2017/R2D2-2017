d7d5f1ca902bbf54a970a782f1250faa90eb22c8
Bob Thomas <bob_mathias_thomas@hotmail.com>
2017-06-14 17:27:54 +0200

    [CARRIER-02] add status led

--- modules/CARRIER/src/main.cc
+++ modules/CARRIER/src/main.cc
@@ -2,34 +2,41 @@
 #include "serial-com.hh"
 #include <wiringPi.h>
 
+
 int main(void){
+	int statusLed = 29;
 	MotorController controller("/dev/ttyS0", 38400);
 	SerialCom serialCom("/dev/rfcomm0", 9600);
-
+	std::string currentCommand = "";
+  	wiringPiSetup();
+	pinMode (statusLed, OUTPUT) ;
 	while(serialCom.init() == 0) {
+		digitalWrite (statusLed, 1) ;     // On
         delay(1000);
+		digitalWrite (statusLed, 0) ;     // off		
     }
+	digitalWrite (statusLed, 1) ;     // On
     while(true){
         std::string command = serialCom.readCommand();
         if(command != "-1") {
             if(command.find("FORWARD") != std::string::npos) {
                 serialCom.write("GOING FORWARD");
 				controller.forward(100);
-				delay(1000);
+				delay(100);
 				controller.stop();
             } else if(command.find("BACKWARD") != std::string::npos) {
                 serialCom.write("GOING BACKWARD");
 				controller.backward(100);
-				delay(1000);
+				delay(100);
 				controller.stop();
             } else if(command.find("LEFT") != std::string::npos) {
 				controller.left(100);
-				delay(1000);
+				delay(100);
 				controller.stop();
 			} else if(command.find("RIGHT") != std::string::npos) {
                 serialCom.write("GOING RIGHT");
 				controller.right(100);
-				delay(1000);
+				delay(100);
 				controller.stop();
             }
             printf("%s", command.c_str());      
