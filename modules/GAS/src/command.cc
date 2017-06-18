2a502946774461656585c3636a11cbc579aefecc
Mike <m.hilhorst@hotmail.nl>
2017-06-18 17:36:42 +0200

    [GAS-03]Made it possible to display a string given to operate. fixed the bug where the displayed string shifted in undefined memory. Got rid of display time, now the display will display it's cuurent value until a new value is pushed towards it + manny other bugs fixed

--- modules/GAS/src/command.cc
+++ modules/GAS/src/command.cc
@@ -4,96 +4,68 @@
 
 #include "command.hh"
 
-void command::commander()
+void command::commander(uint8_t (*commands)[8][2])
 {
-    int tmp = number_of_matrices;
-    uint8_t temp[1000];
-    for(int i = 0; i < string_lenght-tmp;i++)
-    {
-        for(int m = 0; m < count ;m++)
-        {
-            for(int j =0; j <= count; j++)
-            {
-                int l = 0;
-                for(int k = 0; k < number_of_matrices; k++)
-                {
-                    temp[l] = {(uint8_t)j};
-                    l++;
-                    temp[l] = {data[i+k][j-1][1]};
-                    l++;
-                }
-                bus.write_and_read(cs, 2*number_of_matrices, temp , nullptr);
-            }
-            hwlib::wait_ms(time/7);
-        }
-        //hwlib::wait_ms(time);
-        cleaner();
-    }
-}
-
-void command::command_settings(const uint8_t (*data)[2])
-{
-    uint8_t temp[1000];
-    for(int i = 0; i < 5 ;i++) // 5 number of commands
+    for(int j =0; j <= numberOfRows; j++)
     {
         int l = 0;
-        for(int k = 0; k < number_of_matrices; k++)
+        for(int k = 0; k < numberOfMatrices; k++)
         {
-            temp[l] = {data[i][0]};
+            processedCommands[l] = {(uint8_t)j};
             l++;
-            temp[l] = {data[i][1]};
+            processedCommands[l] = {commands[k][j-1][1]};
             l++;
         }
-        bus.write_and_read(cs, 2*number_of_matrices, temp , nullptr);    //data is send over SPI to MAX7219. Only write, read is voided
+        spiBus.write_and_read(chipSelect, 2*numberOfMatrices, processedCommands , nullptr);
     }
 }
 
-
-void command::cleaner()
+void command::settings(const uint8_t (*settingsList)[2])
 {
-    for(int i = 1; i <= count; i++)
+    int numberOfCommands = 5;
+    for(int i = 0; i < numberOfCommands ;i++)
     {
-        int n =0;
-        for(int j = number_of_matrices; j > 0; j--)
+        int l = 0;
+        for(int k = 0; k < numberOfMatrices; k++)
         {
-            walk[n]= (uint8_t)i ;
-            n++;
-            walk[n]= 0x00 ;
-            n++;
+            processedCommands[l] = {settingsList[i][0]};
+            l++;
+            processedCommands[l] = {settingsList[i][1]};
+            l++;
         }
-        bus.write_and_read(cs, 2*number_of_matrices, walk, nullptr);
+        spiBus.write_and_read(chipSelect, 2*numberOfMatrices, processedCommands , nullptr);    //data is send over SPI to MAX7219. Only write, read is voided
     }
 }
 
-void command::coverter(uint8_t (*matrix_temp)[8], uint8_t (*matrix_data)[2])
+void command::converter(uint8_t (*renderInput)[8], uint8_t (*converterOutput)[2])
+
 {
     for(int i = 0; i < 8; i++)                                  // Row 0-7;
     {
-        uint8_t tmp = 0;                                           // An temp uint8_t, in to this uint8_t data will be shifted to make from an matrix row, 1 uint8_t.
-        int counter = 0;                                        // Counter column 7-0;
+        uint8_t shiftVariable = 0;                                           // An temp uint8_t, in to this uint8_t data will be shifted to make from an matrix row, 1 uint8_t.
+        int rowCounter = 0;                                        // Counter column 7-0;
         for(int j = 8; j >= 0; j--)                             // J is an shifter, matrix(_tmp) will be shifted J places in tmp.
         {
-            tmp = tmp | matrix_temp[i][counter-1] << (j);        // and then shifed J times.
-            counter++;                                      // Next column!
+            shiftVariable |= renderInput[i][rowCounter-1] << (j);        // and then shifed J times.
+            rowCounter++;                                      // Next column!
         }                                                       //matrix_data is and 8x2 marix, it will be filled with:
-        matrix_data[i][0] = i+1;                                //Row number. i+1 is because the rows start from 1 and NOT 0.
-        matrix_data[i][1] = tmp;                                //Column data.
+        converterOutput[i][0] = i+1;                                //Row number. i+1 is because the rows start from 1 and NOT 0.
+        converterOutput[i][1] = shiftVariable;                                //Column data.
     }
 
 }
 
-void command::render(uint8_t (*render_input)[8]) {
-    command Aether(bus, cs, array_data, time, number_of_matrices, count, string_lenght);
+void command::render(uint8_t (*renderInput)[8]) {
 
-
-    Aether.coverter(render_input, matrix_data);
-    static int j = 0;
-    for (int i = 0; i < count; i++) {
-        array_data[j][i][1] = matrix_data[i][1];
+    converter(renderInput, converterOutput);
+    static int numberOfWrites = 0;
+    for (int i = 0; i < numberOfRows; i++) {
+        commands[numberOfWrites][i][1] = converterOutput[i][1];
     }
-    j++;
-    if (j == string_lenght) {
-        Aether.commander();
+    numberOfWrites++;
+    if (numberOfWrites == stringLength) {
+        numberOfWrites = 0;
+        commander(commands);
     }
 }
 
--- modules/GAS/src/command.hh
+++ modules/GAS/src/command.hh
@@ -13,32 +13,28 @@
 class command
 {
 private:
-    hwlib::spi_bus & bus; 
-    hwlib::pin_out & cs;
-    uint8_t (*data)[8][2];
-    uint8_t walk[1000];
-    int time;
-    int number_of_matrices;
-    int count;
-    int string_lenght;
-    uint8_t matrix_data[8][2];
-    uint8_t matrix_output[8][8];
-    uint8_t array_data[1000][8][2];
+    hwlib::spi_bus & spiBus;
+    hwlib::pin_out & chipSelect;
+    int numberOfMatrices;
+    int numberOfRows;
+    int stringLength;
+    uint8_t processedCommands[1000];
+    uint8_t converterOutput[8][2];
+    uint8_t commands[1000][8][2];
+
 
 public:
-    command(hwlib::spi_bus & bus, hwlib::pin_out & cs, uint8_t (*data)[8][2], int & time, int & number_of_matrices , int & count, int & string_lenght):
-    bus(bus), cs(cs), data(data), time(time), number_of_matrices(number_of_matrices), count(count), string_lenght(string_lenght)
+    command(hwlib::spi_bus & spiBus, hwlib::pin_out & chipSelect, int & numberOfMatrices , int & numberOfRows, int & stringLength):
+    spiBus(spiBus), chipSelect(chipSelect), numberOfMatrices(numberOfMatrices), numberOfRows(numberOfRows), stringLength(stringLength)
     {};
 
-    void commander();
+    void commander(uint8_t (*commands)[8][2]);
 
-    void command_settings(const uint8_t (*data)[2]);
+    void settings(const uint8_t (*settingsList)[2]);
 
-    void cleaner();
+    void converter(uint8_t (*renderInput)[8], uint8_t (*converterOutput)[2]);
 
-    void coverter(uint8_t (*matrix_temp)[8], uint8_t (*matrix_data)[2]);
-
-    void render(uint8_t (*render_input)[8]);
+    void render(uint8_t (*renderInput)[8]);
 
 };
 
