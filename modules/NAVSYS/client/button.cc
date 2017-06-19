853a54ef15a47d6b04c19ffe21565befdd0a3d6a
renedekluis <rdekluis@hotmail.com>
2017-06-19 15:12:24 +0200

    [NAVSYS-03] Fixed pullrequest comments

--- modules/NAVSYS/client/button.cc
+++ modules/NAVSYS/client/button.cc
@@ -19,7 +19,7 @@ Button::Button(sf::RenderWindow & window, sf::Vector2f position,
 {
     button.setSize(size);
     button.setOutlineColor(sf::Color::Red);
-    button.setOutlineThickness(2);
+    button.setOutlineThickness(outlineThickness);
     button.setPosition(position);
 
     if (!font.loadFromFile(fontName)) {
@@ -30,8 +30,8 @@ Button::Button(sf::RenderWindow & window, sf::Vector2f position,
     buttonText.setString(text);
     buttonText.setColor(sf::Color::Black);
     buttonText.setScale(
-        float(((1 / buttonText.getGlobalBounds().width) * (size.x-(size.x/20)))),
-        float((1 / buttonText.getGlobalBounds().height) * size.y/2)
+        ((1 / buttonText.getGlobalBounds().width) * (size.x-(size.x/xScaleDivider))),
+        ((1 / buttonText.getGlobalBounds().height) * (size.y/yScaleDivider))
     );
 
     if (!isVisable) {
@@ -44,24 +44,20 @@ Button::Button(sf::RenderWindow & window, sf::Vector2f position,
 
 void Button::draw() {
     if (isVisable) {
-        if (isFocused) {
+        if (pressedState) {
             button.setFillColor(sf::Color::Blue);
         }
         else {
             button.setFillColor(sf::Color::White);
         }
     }
-    isFocused = false;
+    pressedState = false;
     window.draw(button);
     window.draw(buttonText);
 }
 
-void Button::setFocus(bool b) {
-    isFocused = b;
-}
-
-bool Button::getFocus() {
-    return isFocused;
+void Button::setState(bool b) {
+    pressedState = b;
 }
 
 void Button::setSize(sf::Vector2f newSize) {
@@ -99,10 +95,10 @@ void Button::setVisable(bool visable) {
 bool Button::isPressed() {
     if (getBounds().contains(getMousePosition(window))) {
         if (button.getFillColor() != sf::Color::Transparent) {
-            isFocused = true;
+            pressedState = true;
         }
     }
-    return isFocused;
+    return pressedState;
 }
 
 sf::Vector2f Button::getPosition(){
