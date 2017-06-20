c3a0d00788adbeeec49b8ba817b0eb1f72f0494c
wartaal55 <wilco@louwerse.com>
2017-06-20 12:17:27 +0200

    [GAS-03] Updated/Added doxygen documentation for the parser class.

--- modules/GAS/src/parser.hh
+++ modules/GAS/src/parser.hh
@@ -1,11 +1,11 @@
 /**
  * \file
- * \brief     The declarations of the class Parser of GAS-03, used for ...
+ * \brief     The declarations of the class Parser of GAS-03, used for reading and setting the config variables
+ *            from an given array with these variables.
  * \author    Wilco Louwerse
  * \author	  Nicky van Steensel van der Aa
  * \copyright Copyright (c) 2017, The R2D2 Team
- * \license   See LICENSE
- * \wiki      ...
+ * \license   See LICENSES
  * \trello    https://trello.com/c/zSJnP0Rw/5-a-parser-must-be-made-that-can-read-the-conftxt-and-set-the-variables
  */
 
@@ -19,22 +19,33 @@ class Parser {
 public:
 
     /**
-     * \brief Constructor for Parser
+     * \brief Constructor for Parser.
+     * \param alarm                 This is the alarm object where some of the alarm related settings will be set.
+     * \param mq5                   This is the mq5 object where some of the mq-5 sensor related settings will be set.
+     * \param measureWaitTime       This is a pointer to a setting value in the main that will be set.
      */
-    Parser(Alarm &alarm, Mq5 &mq5, int *measurmentWaitTime);
+    Parser(Alarm &alarm, Mq5 &mq5, int *measureWaitTime);
 
     /**
-     * \brief
-     * @param input the input to parse
+     * \brief Read and set the settings from a read given input array.
+     * \param input the input array to parse.
      */
     void parseArray(char *input);
 
 private:
 
+    /**
+     * \brief Checks if a given array contains a given string.
+     * \param array     The given array to check with.
+     * \param string    The given string to check with.
+     * \return          Returns true if the given array contains the given string.
+     */
+    bool ifContainsString(char array[], const char *string);
+
     Alarm &alarm;
     Mq5 &mq5;
 
-    int *measurmentWaitTime;
+    int *measureWaitTime;
     char variableName[20];
     int variableValue = 0;
 
@@ -45,7 +56,5 @@ private:
     const char *mq5CalibrationValueString   = "mq5CalibrationValue";
     const char *measureWaitTimeString       = "measureWaitTime";
 	const char *isCalibratedString          = "isCalibrated";
-
-    bool ifContainsString(char array[], const char *string);
 
 };
